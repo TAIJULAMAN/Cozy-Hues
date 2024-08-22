5be55b338bf97ff0123efff55326f6d2e9d3a0d1
TAIJULAMAN <taijulaman8@gmail.com>
2024-08-23 00:03:57 +0600

    edit route

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,104 +1,92 @@
-import {
-    createBrowserRouter,
-  } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main/Main";
 import Home from "../Pages/Home/Home";
 import Products from "../Pages/Products/Products";
-import Categories from "../Pages/Categories/Categories";
 import Blogs from "../Pages/Blog/Blogs";
 
-
-
-
-  export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main/>,
-      children: [
-        {
-            path: "/",
-            element: <Home/>
-        },
-        {
-            path: "/products",
-            element:<Products/>
-        },
-        {
-          path: "/categories",
-          element:<Categories/>
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products",
+        element: <Products />,
       },
       {
         path: "/blogs",
-        element:<Blogs/>
-    },
-    //     {
-    //         path: "/order/:category",
-    //         element:<OrderPage></OrderPage>
-    //     },
-    //     {
-    //       path: "/login",
-    //       element:<LogIn></LogIn>
-    //   },
-    //     {
-    //       path: "/signup",
-    //       element:<Signup></Signup>
-    //   },
-    //     {
-    //       path: "gallery",
-    //       element:<Gallery></Gallery>
-    //   }
+        element: <Blogs />,
+      },
+      //     {
+      //         path: "/order/:category",
+      //         element:<OrderPage></OrderPage>
+      //     },
+      //     {
+      //       path: "/login",
+      //       element:<LogIn></LogIn>
+      //   },
+      //     {
+      //       path: "/signup",
+      //       element:<Signup></Signup>
+      //   },
+      //     {
+      //       path: "gallery",
+      //       element:<Gallery></Gallery>
+      //   }
+    ],
+  },
 
-      ]
-    },
+  // dashboard routes..............................................................................
 
-    // dashboard routes..............................................................................
-
-    // {
-    //   path: "dashboard",
-    //   element:<PrivateRoute><DashBoard></DashBoard></PrivateRoute>,
-    //   children: [
-    //     {
-    //       path: "mycart",
-    //       element:<MyCart></MyCart>,
-    //     },
-    //     {
-    //       path: "userhome",
-    //       element:<UserHome></UserHome>
-    //     },
-    //     {
-    //       path: "reservation",
-    //       element:<Reservation></Reservation>
-    //     },
-    //     {
-    //       path: "payment",
-    //       element:<Payment></Payment>
-    //     },
-    //     {
-    //       path: "reviews",
-    //       element:<AddReview></AddReview>
-    //     },
-    //     {
-    //       path: "bookings",
-    //       element:<Bookings></Bookings>
-    //     },
-    //     // ...................admin route.........................
-    //     {
-    //       path: "allusers",
-    //       element:<AdminRoute><AllUsers></AllUsers></AdminRoute>
-    //     },
-    //     {
-    //       path: "addItem",
-    //       element:<AdminRoute><AddItem></AddItem></AdminRoute>
-    //     },
-    //     {
-    //       path: "manageitem",
-    //       element:<AdminRoute><ManageItem></ManageItem></AdminRoute>
-    //     },
-    //     {
-    //       path: "adminhome",
-    //       element:<AdminRoute><AdminHome></AdminHome></AdminRoute>
-    //     },
-    //   ]
-    // }
-  ]);
-  
+  // {
+  //   path: "dashboard",
+  //   element:<PrivateRoute><DashBoard></DashBoard></PrivateRoute>,
+  //   children: [
+  //     {
+  //       path: "mycart",
+  //       element:<MyCart></MyCart>,
+  //     },
+  //     {
+  //       path: "userhome",
+  //       element:<UserHome></UserHome>
+  //     },
+  //     {
+  //       path: "reservation",
+  //       element:<Reservation></Reservation>
+  //     },
+  //     {
+  //       path: "payment",
+  //       element:<Payment></Payment>
+  //     },
+  //     {
+  //       path: "reviews",
+  //       element:<AddReview></AddReview>
+  //     },
+  //     {
+  //       path: "bookings",
+  //       element:<Bookings></Bookings>
+  //     },
+  //     // ...................admin route.........................
+  //     {
+  //       path: "allusers",
+  //       element:<AdminRoute><AllUsers></AllUsers></AdminRoute>
+  //     },
+  //     {
+  //       path: "addItem",
+  //       element:<AdminRoute><AddItem></AddItem></AdminRoute>
+  //     },
+  //     {
+  //       path: "manageitem",
+  //       element:<AdminRoute><ManageItem></ManageItem></AdminRoute>
+  //     },
+  //     {
+  //       path: "adminhome",
+  //       element:<AdminRoute><AdminHome></AdminHome></AdminRoute>
+  //     },
+  //   ]
+  // }
+]);
